909afa7497c30f0c287fc2b4bc7ed20293df3c90
Kalsash <kalsash@yandex.ru>
2025-11-06 21:49:44 +0300

    lab4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,6 +90,8 @@ dependencies {
     //Serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.1")
 
+    implementation("com.commonsware.cwac:saferoom.x:1.2.1")
+
     // Testing
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
